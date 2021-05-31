143650de08c063154bbf40c7a1042b7ded9affd3
AlessandroVe <aveneziano536@gmail.com>
2021-05-31 23:08:24 +0200

    .

---
+++ js/script.js
@@ -0,0 +1,18 @@
+mailInput=prompt("inserisci qui la tua mail");
+regidit=["mailgiusta@gmail.com","mail@gmail.com","giusta@gmail.com"];
+if(mailInput===regidit[0]){
+    document.getElementById("output").innerHTML="giusta";
+}else if(mailInput===regidit[1]){
+    document.getElementById("output").innerHTML="giusta";
+}else if(mailInput===regidit[2]){
+    document.getElementById("output").innerHTML="giusta";
+}else{
+    document.getElementById("output").innerHTML="sbagliata";
+}
+
+/* 
+
+/ |
+\ |
+
+ */
