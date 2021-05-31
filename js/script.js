0e25598cc7fd50bd69c595056183dab2a12231bc
AlessandroVe <aveneziano536@gmail.com>
2021-05-31 23:13:04 +0200

    .

--- js/script.js
+++ js/script.js
@@ -1,13 +1,14 @@
 mailInput=prompt("inserisci qui la tua mail");
 regidit=["mailgiusta@gmail.com","mail@gmail.com","giusta@gmail.com"];
+var output=document.getElementById("output");
 if(mailInput===regidit[0]){
-    document.getElementById("output").innerHTML="giusta";
+    output.innerHTML="giusta";
 }else if(mailInput===regidit[1]){
-    document.getElementById("output").innerHTML="giusta";
+    output.innerHTML="giusta";
 }else if(mailInput===regidit[2]){
-    document.getElementById("output").innerHTML="giusta";
+    output.innerHTML="giusta";
 }else{
-    document.getElementById("output").innerHTML="sbagliata";
+    output.innerHTML="sbagliata";
 }
 
 /* 
